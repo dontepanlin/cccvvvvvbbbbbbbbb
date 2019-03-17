bb20a41b59a6228529f3017d3fe74b96f7e625fc
unclehook <vladislavegorov555@gmail.com>
2019-03-17 12:59:13 +0300

    Lab4 done

--- kern/kclock.c
+++ kern/kclock.c
@@ -9,6 +9,18 @@ rtc_init(void)
 	nmi_disable();
 	// LAB 4: your code here
 
+	outb(IO_RTC_CMND, RTC_BREG);
+
+	uint8_t reg_b = inb(IO_RTC_DATA);
+	reg_b |= RTC_PIE;
+	outb(IO_RTC_CMND, RTC_BREG);
+	outb(IO_RTC_DATA, reg_b);
+	outb(IO_RTC_CMND, RTC_BREG);
+	uint8_t reg_a = inb(IO_RTC_DATA);
+
+	reg_a |= 0x0F;
+	outb(IO_RTC_CMND, RTC_AREG);
+	outb(IO_RTC_DATA, reg_a);
 	nmi_enable();
 }
 
@@ -17,6 +29,13 @@ rtc_check_status(void)
 {
 	uint8_t status = 0;
 	// LAB 4: your code here
+	nmi_disable();
+
+	outb(IO_RTC_CMND, RTC_CREG);
+	status = inb(IO_RTC_DATA);
+
+	nmi_enable();
+
 
 	return status;
 }
